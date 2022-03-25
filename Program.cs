5a4665962ace7ad790afc80729d4f29d9baf15ca
Александр Литвинов <sbawalexlit2@gmail.com>
2022-03-25 20:23:57 +0300

    program

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.WriteLine("введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите степень");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = 1;
+// for (int i = 0; i <b; i ++)
+// {
+// c= c*a;
+// }
+// Console.WriteLine(c);
+
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = 0;
+// int sum = 0;
+// while (a > 0)
+// {
+//     b = a % 10;
+//     sum = sum + b;
+//     a = a / 10;
+// }
+// Console.WriteLine(sum);
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int[] mass = new int[8];
+// Random newMass = new Random();
+// for (int i = 0; i < mass.Length; i++ )
+// {
+// mass[i]= newMass.Next(0,10);
+// Console.Write($" {mass[i]}");
+// }
